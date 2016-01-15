4a80a9781a805c69fe397ab8c0952ae1a13ca525
Patryk Lotzwi <p.lotzwi@outlook.com>
2016-01-15 20:45:55 +0100

    Added test run script

---
+++ Nocturno/RunTests.ps1
@@ -0,0 +1,9 @@
+Set-ExecutionPolicy unrestricted -Scope CurrentUser -Force
+
+#dnvm setup
+
+#dnvm use default
+
+#dnx $PSScriptRoot\src\ClassLibrary4 test.\nxslt2.exe $PSScriptRoot\testresults.xml .\NunitXslt.xslt -o testresultstransformed.xml
+
+Get-ChildItem -Path $PSScriptRoot\test -Filter project.json -Recurse | ForEach-Object { & dnx test }
