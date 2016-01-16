37b039787aece48814b2c8529d4f7eb75bd402b7
Patryk Lotzwi <p.lotzwi@outlook.com>
2016-01-16 13:52:33 +0100

    yet another fix

--- Nocturno/RunTests.ps1
+++ Nocturno/RunTests.ps1
@@ -4,4 +4,4 @@ dnvm install latest
 
 #dnx $PSScriptRoot\src\ClassLibrary4 test.\nxslt2.exe $PSScriptRoot\testresults.xml .\NunitXslt.xslt -o testresultstransformed.xml
 
-Get-ChildItem -Path $PSScriptRoot\test -Filter project.json -Recurse | ForEach-Object { & dnx test }
+Get-ChildItem -Path $PSScriptRoot\test -Filter project.json -Recurse | ForEach-Object { & dnx -p $_.FullName test }
