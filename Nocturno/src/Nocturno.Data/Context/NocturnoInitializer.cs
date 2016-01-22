2eb78f32eec5d805fdf601cb096c51686da73672
Patryk Lotzwi <patryk.lotzwi@capgemini.com>
2016-01-22 23:24:33 +0100

    added contentype service

---
+++ Nocturno/src/Nocturno.Data/Context/NocturnoInitializer.cs
@@ -0,0 +1,52 @@
+﻿using Nocturno.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Nocturno.Data.Context
+{
+    public class NocturnoInitializer : IDbInitializer
+    {
+        private readonly IDbContext _db;
+
+        public NocturnoInitializer(IDbContext db)
+        {
+            _db = db;
+        }
+
+        public void InitializeDatabase()
+        {
+            if (!_db.Sections.Any())
+            {
+                var sections = new List<Section>
+                {
+                    new Section { Name = "Navigation"},
+                    new Section { Name = "Header Top"},
+                    new Section { Name = "Header Bottom"},
+                    new Section { Name = "Breadcrumb"},
+                    new Section { Name = "Main Top"},
+                    new Section { Name = "Main Middle"},
+                    new Section { Name = "Main Bottom"},
+                    new Section { Name = "Left sidebar"},
+                    new Section { Name = "Right sidebar"},
+                    new Section { Name = "Footer Top"},
+                    new Section { Name = "Footer Bottom"},
+                };
+                _db.Sections.AddRange(sections);
+                _db.SaveChanges();
+            }
+
+            if (!_db.Menus.Any())
+            {
+                _db.Menus.Add(new Menu
+                {
+                    Name = "Main",
+                    SectionId = _db.Sections.FirstOrDefault(x => x.Name == "Navigation").Id
+                });
+                _db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Nocturno/src/Nocturno.Service/Services/ContentTypeService.cs
@@ -0,0 +1,34 @@
+﻿using Nocturno.Data.Context;
+using Nocturno.Data.Models;
+using Nocturno.Service.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nocturno.Service.Services
+{
+    public class ContentTypeService : BaseService<CmsContentType>, IContentTypeService
+    {
+        public ContentTypeService(IDbContext db) : base(db)
+        {
+        }
+
+        public void AddField(CmsContentType contentType, CmsFieldType field, int order)
+        {
+            _db.CmsContentTypeToFieldTypes.Add(new CmsContentTypeToFieldType
+            {
+                CmsContentTypeId = contentType.Id,
+                CmsFieldTypeId = field.Id,
+                Order = order
+            });
+        }
+
+        public void RemoveField(CmsContentType contentType, CmsFieldType field)
+        {
+            var record = _db.CmsContentTypeToFieldTypes
+                .FirstOrDefault(x => x.CmsContentTypeId == contentType.Id && x.CmsFieldTypeId == field.Id);
+            _db.CmsContentTypeToFieldTypes.Remove(record);
+        }
+    }
+}
